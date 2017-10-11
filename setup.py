ad6eabb224aa582c0e6ee261964a42ab87446180
Terry Cain <terry@terrys-home.co.uk>
2017-10-11 22:56:44 +0100

    Bump version: 1.0.0 → 1.1.0

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ test_requirements = [
 
 setup(
     name='aioboto3',
-    version='1.0.0',
+    version='1.1.0',
     description="Async boto3 wrapper",
     long_description=readme + '\n\n' + history,
     author="Terry Cain",
