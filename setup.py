eec09d2e4704b62f87ffc473ef789db6e0221c3e
Terry Cain <TerryCain@fico.com>
2017-12-01 09:33:50 +0000

    Bump version: 2.0.0 → 2.0.1

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ test_requirements = [
 
 setup(
     name='aioboto3',
-    version='2.0.0',
+    version='2.0.1',
     description="Async boto3 wrapper",
     long_description=readme + '\n\n' + history,
     author="Terry Cain",
