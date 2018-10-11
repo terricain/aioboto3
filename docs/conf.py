a57275c40e3e3cf87de95d2211ee24bbfcc04e94
Terry Cain <terry@terrys-home.co.uk>
2018-10-12 00:27:28 +0100

    Revert earlier changes

--- docs/conf.py
+++ docs/conf.py
@@ -274,4 +274,4 @@ texinfo_documents = [
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
 
-autodoc_mock_imports = ['aiohttp']
+# autodoc_mock_imports = ['aiohttp']
