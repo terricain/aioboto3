0b2dff039d3089d30702becd7df41862786a6c99
Andrew Rabert <ar@nullsum.net>
2018-12-05 21:14:27 -0500

    Fix Sphinx warnings - static directory does not exist - modules was not defined

--- docs/conf.py
+++ docs/conf.py
@@ -138,12 +138,6 @@ html_theme = 'default'
 # 16x16 or 32x32 pixels large.
 #html_favicon = None
 
-# Add any paths that contain custom static files (such as style sheets)
-# here, relative to this directory. They are copied after the builtin
-# static files, so a file named "default.css" will overwrite the builtin
-# "default.css".
-html_static_path = ['_static']
-
 # If not '', a 'Last updated on:' timestamp is inserted at every page
 # bottom, using the given strftime format.
 #html_last_updated_fmt = '%b %d, %Y'
--- docs/index.rst
+++ docs/index.rst
@@ -9,7 +9,6 @@ Contents:
    readme
    installation
    usage
-   modules
    contributing
    authors
    history
