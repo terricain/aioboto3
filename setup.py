d1bf946cf5cd1b994b4ba2b8987eeaceb665a510
Terry Cain <terry@terrys-home.co.uk>
2020-04-08 01:11:22 +0100

    Bump version: 8.0.0 → 8.0.1

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ extras_require = {
 
 setup(
     name='aioboto3',
-    version='8.0.0',
+    version='8.0.1',
     description="Async boto3 wrapper",
     long_description=readme + '\n\n' + history,
     author="Terry Cain",
