9243dd145df8b13c823526b9eb10b3dcfae829d4
terrycain <terrycain@users.noreply.github.com>
2020-06-07 20:13:39 +0000

    Auto-committed changes by create-pull-request action

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = [
-    'aiobotocore[boto3]==1.0.4'
+    'aiobotocore[boto3]==1.0.7'
 ]
 
 setup_requirements = [
