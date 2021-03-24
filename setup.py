185ad43aa4aaa59826623762c56b744eea232959
Terry Cain <terry@terrys-home.co.uk>
2021-03-24 23:00:09 +0000

    Bumped aiobotocore patch version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = [
-    'aiobotocore[boto3]==1.2.1'
+    'aiobotocore[boto3]==1.2.2'
 ]
 
 setup_requirements = [
